47bdc523943054dc13903e5109cd81dc50fa565b
Anatol V <malfenius@gmail.com>
2020-01-26 18:07:02 +0300

    Task-2. Реализовал сущность для хранения информации о файле

--- src/main/java/ru/inspector_files/model/Document.java
+++ src/main/java/ru/inspector_files/model/Document.java
@@ -1,22 +1,28 @@
-package ru.inspectorfiles.model;
+package ru.inspector_files.model;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
+import java.time.LocalDateTime;
 
 @Entity
-@Table(name = "file")
+@Table(name = "file_storage")
 public class Document {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
     @NotBlank
-    @Column(name = "absolute_path", nullable = false, unique = true)
+    @Column(name = "absolute_path", nullable = false, length = 100_000)
     private String path;
-    @Column(name = "file_name")
+    @Column
     private String name;
-    @Column(name = "size")
-    private int size;
+    @Column
+    private long size;
     @Column(name = "check_sum")
     private String checkSum;
+    @Column(name = "create_time", nullable = false)
+    private LocalDateTime createTime;
+    @Column(name = "content_type")
+    private String contentType;
 
     public Document() {
     }
@@ -37,11 +43,11 @@ public class Document {
         this.name = name;
     }
 
-    public int getSize() {
+    public long getSize() {
         return size;
     }
 
-    public void setSize(int size) {
+    public void setSize(long size) {
         this.size = size;
     }
 
@@ -53,6 +59,22 @@ public class Document {
         this.checkSum = checkSum;
     }
 
+    public LocalDateTime getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(LocalDateTime createTime) {
+        this.createTime = createTime;
+    }
+
+    public String getContentType() {
+        return contentType;
+    }
+
+    public void setContentType(String contentType) {
+        this.contentType = contentType;
+    }
+
     @Override
     public String toString() {
         return "Document{" +
@@ -61,5 +83,9 @@ public class Document {
                 ", size=" + size +
                 ", checkSum='" + checkSum + '\'' +
                 '}';
+    }
+
+    public boolean isNew() {
+        return id == null;
     }
 }

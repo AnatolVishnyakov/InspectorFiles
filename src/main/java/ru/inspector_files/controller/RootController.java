4e68edee3cc5187d09e62b5bf72a9a852e655ab3
Anatol V <malfenius@gmail.com>
2020-02-16 21:35:30 +0300

    Task-14. Добавил кнопки закрытия/сворачивания приложения

--- src/main/java/ru/inspector_files/controller/RootController.java
+++ src/main/java/ru/inspector_files/controller/RootController.java
@@ -1,15 +1,25 @@
 package ru.inspector_files.controller;
 
+import com.jfoenix.controls.JFXButton;
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.stage.Stage;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class RootController extends AbstractController implements Initializable {
+    @FXML
+    private JFXButton buttonClose;
+    @FXML
+    private JFXButton buttonMinimize;
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        buttonClose.addEventHandler(ActionEvent.ANY, mouseEvent -> Platform.exit());
+        buttonMinimize.addEventHandler(ActionEvent.ANY, mouseEvent -> ((Stage) buttonMinimize.getScene().getWindow()).setIconified(true));
         onDashboardButtonClick(new ActionEvent());
     }
 

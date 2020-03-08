4092387a81f7c598d0018bf89b485935c78c3461
Anatol V <malfenius@gmail.com>
2020-03-08 21:06:36 +0300

    Task-20. Залогировал иерархию файлов, повысил скорость развертывания дочерних элементов

--- src/main/java/ru/inspector_files/ui/controls/FolderTree.java
+++ src/main/java/ru/inspector_files/ui/controls/FolderTree.java
@@ -12,12 +12,12 @@ import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
-public class FolderTreeItem extends CheckBoxTreeItem<File> {
+public class FolderTree extends CheckBoxTreeItem<File> {
     private static final Logger logger = LoggerFactory.getLogger("logger.debug");
     private static final File[] EMPTY_ARRAY = {};
     private static final Set<File> selectedFolders = new HashSet<>();
 
-    public FolderTreeItem(File file) {
+    public FolderTree(File file) {
         super(new File(file.getAbsolutePath()) {
             @Override
             public String toString() {
@@ -28,49 +28,45 @@ public class FolderTreeItem extends CheckBoxTreeItem<File> {
             }
         });
         this.expandedProperty().addListener((observable, oldValue, newValue) -> {
+            logger.debug("expanded");
             super.getChildren().setAll(buildChildren(this));
             TreeItem<File> parent = getParent();
             if (parent.getValue() == null) {
+                logger.debug("remove parent: {}", getValue());
                 selectedFolders.remove(getValue());
-                System.out.println("remove: " + getValue());
             }
         });
         this.selectedProperty().addListener((observable, oldValue, newValue) -> {
             if (newValue) {
+                logger.debug("selected value: {}", getValue());
                 selectedFolders.add(getValue());
             } else {
+                logger.debug("unselected value: {}", getValue());
                 selectedFolders.remove(getValue());
             }
         });
-    }
-
-    @Override
-    public ObservableList<TreeItem<File>> getChildren() {
-        logger.debug("getChildren: {}", getValue());
-        return super.getChildren();
     }
 
     @Override
     public boolean isLeaf() {
         logger.debug("isLeaf: {}", getValue());
-        File[] files = getValue().listFiles(File::isDirectory);
-        return files == null || files.length == 0;
+        return !getValue().isDirectory();
     }
 
     private ObservableList<CheckBoxTreeItem<File>> buildChildren(CheckBoxTreeItem<File> item) {
         logger.debug("buildChildren: {}", item.getValue());
         File currentFile = item.getValue();
-        if (hasFolders(currentFile)) {
+        if (currentFile.isDirectory()) {
             ObservableList<CheckBoxTreeItem<File>> children = FXCollections.observableArrayList();
 
             File[] folders = getFolders(currentFile);
             for (File childFolder : folders) {
-                if (hasFolders(childFolder)) {
-                    FolderTreeItem folderTreeItem = new FolderTreeItem(childFolder);
+                if (childFolder.isDirectory()) {
+                    FolderTree folderTree = new FolderTree(childFolder);
                     if (item.isSelected()) {
-                        folderTreeItem.setSelected(true);
+                        folderTree.setSelected(true);
                     }
-                    children.add(folderTreeItem);
+                    children.add(folderTree);
                 }
             }
 
@@ -79,30 +75,23 @@ public class FolderTreeItem extends CheckBoxTreeItem<File> {
         return FXCollections.emptyObservableList();
     }
 
-    private boolean hasFolders(File folder) {
-        if (folder != null) {
-            File[] files = getFolders(folder);
-            return files != null && files.length > 0;
-        }
-        return false;
-    }
-
     private File[] getFolders(File folder) {
+        logger.debug("getFolders: {}", folder);
         if (folder != null) {
             File[] files = folder.listFiles();
             if (files != null && files.length > 0) {
                 Object[] objects = Arrays.stream(files)
                         .filter(File::isDirectory)
                         .toArray();
-                File[] results = new File[objects.length];
+                Object[] results = new File[objects.length];
                 System.arraycopy(objects, 0, results, 0, objects.length);
-                return results;
+                return (File[]) results;
             }
         }
         return EMPTY_ARRAY;
     }
 
-    public Set<File> getSelectedFolders() {
+    public static Set<File> getSelectedFolders() {
         return selectedFolders;
     }
 }
